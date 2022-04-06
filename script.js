cd67d29d6b5cffe5ee091392e11c470b2a0b9f90
Alex Running <runniale000@gmail.com>
2022-04-05 22:59:01 -0500

    initial commit ft. APOD API in script

---
+++ script.js
@@ -0,0 +1,13 @@
+var photo_key = 'UaWaPX64ifKiFlemQ73fWGXNMRw5uwk1H5Bt5Feg'
+//https://api.nasa.gov/planetary/apod?api_key=UaWaPX64ifKiFlemQ73fWGXNMRw5uwk1H5Bt5Feg
+//fetch('https://api.nasa.gov/planetary/apod?api_key=count')
+    
+//    .then(function (repsonse) {
+//        return repsonse.json()
+//    })
+//    .then(function (data) {
+//        console.log(data[0])
+//    })
+//    .catch(function(error) {
+//        console.log(error)
+//    })
