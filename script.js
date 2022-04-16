3805d8fe58f644d3750a803fbe996d1bdff4417a
Aubree Alexander <aubree.alexander@gmail.com>
2022-04-15 19:01:36 -0500

    add .each function to history button functionality

--- script.js
+++ script.js
@@ -65,20 +65,19 @@ function getHistory() {
       };
 
   // displays city info once histBtn is clicked
-  $('.histBtn').each(function() {
+ 
     $('.histBtn').click(function(event) {
-        event.preventDefault();
-        $("#forecastContainer").empty();
-        var histSearch = $(".histBtn").text()
-        console.log(histSearch)
-        
-        
-        // weatherForecast(histSearch)
-        // zipCode = $(this).text();
-        // forecastContainer.empty();
-        // weatherForecast();
-    });
-  })
+      $('.histBtn').each(function() {
+            event.preventDefault();
+            $("#forecastContainer").empty();
+            var histSearch = $(".histBtn").text()
+            console.log(histSearch)
+            // weatherForecast(histSearch)
+            // zipCode = $(this).text();
+            // forecastContainer.empty();
+            // weatherForecast();
+        });
+    })
 };
 
 // to pull and display weather forecast
